3fc460b5e530842f83c94e9717d1d187f6b1e0db
Michael Sasser <Michael@MichaelSasser.org>
2021-01-15 00:38:11 +0100

    Finish trigger -> slope

--- ds2000/controller/trigger_subcontrollers/slope.py
+++ ds2000/controller/trigger_subcontrollers/slope.py
@@ -14,8 +14,13 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from ds2000.controller import SubController, SubSubController
+from __future__ import annotations
+from ds2000.controller import (
+    SubController,
+    SubSubController,
+    check_input,
+    check_level,
+)
 
 __author__ = "Michael Sasser"
 __email__ = "Michael@MichaelSasser.org"
@@ -27,7 +32,9 @@ __all__ = [
 
 # ToDo: shorter method names.
 class SlopeWhen(SubSubController):
-    def positive_slope_time_greater_than_specified_lower_limit(self):
+    def set_positive_slope_time_greater_than_specified_lower_limit(
+        self,
+    ) -> None:
         """
         **Rigol Programming Guide**
 
@@ -93,9 +100,9 @@ class SlopeWhen(SubSubController):
         :TRIGger:SLOPe:WHEN PGReater
         The query returns PGR.
         """
-        raise NotImplementedError()
+        self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WHEN PGReater")
 
-    def positive_slope_time_lower_than_specified_upper_limit(self):
+    def set_positive_slope_time_lower_than_specified_upper_limit(self) -> None:
         """
         **Rigol Programming Guide**
 
@@ -161,9 +168,11 @@ class SlopeWhen(SubSubController):
         :TRIGger:SLOPe:WHEN PGReater
         The query returns PGR.
         """
-        raise NotImplementedError()
+        self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WHEN PLESs")
 
-    def negative_slope_time_greater_than_specified_lower_limit(self):
+    def set_negative_slope_time_greater_than_specified_lower_limit(
+        self,
+    ) -> None:
         """
         **Rigol Programming Guide**
 
@@ -229,9 +238,9 @@ class SlopeWhen(SubSubController):
         :TRIGger:SLOPe:WHEN PGReater
         The query returns PGR.
         """
-        raise NotImplementedError()
+        self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WHEN NGReater")
 
-    def negative_slope_time_lower_than_specified_upper_limit(self):
+    def set_negative_slope_time_lower_than_specified_upper_limit(self) -> None:
         """
         **Rigol Programming Guide**
 
@@ -297,9 +306,11 @@ class SlopeWhen(SubSubController):
         :TRIGger:SLOPe:WHEN PGReater
         The query returns PGR.
         """
-        raise NotImplementedError()
+        self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WHEN NLESs")
 
-    def positive_slope_time_between_specified_lower_and_upper_limit(self):
+    def set_positive_slope_time_between_specified_lower_and_upper_limit(
+        self,
+    ) -> None:
         """
         **Rigol Programming Guide**
 
@@ -365,9 +376,11 @@ class SlopeWhen(SubSubController):
         :TRIGger:SLOPe:WHEN PGReater
         The query returns PGR.
         """
-        raise NotImplementedError()
+        self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WHEN PGLess")
 
-    def negative_slope_time_between_specified_lower_and_upper_limit(self):
+    def set_negative_slope_time_between_specified_lower_and_upper_limit(
+        self,
+    ) -> None:
         """
         **Rigol Programming Guide**
 
@@ -433,12 +446,80 @@ class SlopeWhen(SubSubController):
         :TRIGger:SLOPe:WHEN PGReater
         The query returns PGR.
         """
-        raise NotImplementedError()
+        self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WHEN NGLess")
+
+    def status(self) -> str:
+        """
+        **Rigol Programming Guide**
+
+        **Syntax**
+
+        :TRIGger:SLOPe:WHEN <when>
+        :TRIGger:SLOPe:WHEN?
+
+        **Description**
+
+        Select the trigger condition of slope trigger.
+        Query the current trigger condition of slope trigger.
+
+        **Parameter**
+
+        ======= ========= ========================= ========
+        Name    Type      Range                     Default
+        ======= ========= ========================= ========
+        <when>  Discrete  {PGReater|PLESs|NGReater  PGReater
+                          |NLESs|PGLess|NGLess}
+        ======= ========= ========================= ========
+
+        **Explanation**
+
+        PGReater: you need to specify a time value (refer to the
+        :TRIGger:SLOPe:TLOWer command). The oscilloscope triggers when the
+        positive slope time of the input signal is greater than the specified
+        time.
+
+        PLESs: you need to specify a time value (refer to the
+        :TRIGger:SLOPe:TUPPer command). The oscilloscope triggers when the
+        positive slope time of the input signal is lower than the specified
+        time.
+
+        NGReater: you need to specify a time value (refer to the
+        :TRIGger:SLOPe:TLOWer command). The oscilloscope triggers when the
+        negative slope time of the input signal is greater than the specified
+        time.
+
+        NLESs: you need to specify a time value (refer to the
+        :TRIGger:SLOPe:TUPPer command). The oscilloscope triggers when the
+        negative slope time of the input signal is lower than the specified
+        time.
+
+        PGLess: you need to specify an upper limit (refer to the
+        :TRIGger:SLOPe:TUPPer command) and a lower limit (refer to the
+        :TRIGger:SLOPe:TLOWer command) of time. The oscilloscope triggers when
+        the positive slope time of the input signal is greater than the
+        specified lower limit and lower than the specified upper limit.
+
+        NGLess: you need to specify an upper limit (refer to the
+        :TRIGger:SLOPe:TUPPer command) and a lower limit (refer to the
+        :TRIGger:SLOPe:TLOWercommand) of time. The oscilloscope triggers when
+        the negative slope time of the input signal is greater than the
+        specified lower limit and lower than the specified upper limit.
+
+        **Return Format**
+
+        The query returns PGR, PLES, NGR, NLES, PGL or NGL.
+
+        **Example**
+
+        :TRIGger:SLOPe:WHEN PGReater
+        The query returns PGR.
+        """
+        return self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WHEN?")
 
 
 # ToDo: shorter method names.
 class SlopeWindow(SubSubController):
-    def adjust_upper_limit_of_trigger_level(self):
+    def set_adjust_upper_limit_of_trigger_level(self) -> None:
         """
         **Rigol Programming Guide**
 
@@ -484,9 +565,9 @@ class SlopeWindow(SubSubController):
         :TRIGger:SLOPe:WINDow TB
         The query returns TB.
         """
-        raise NotImplementedError()
+        self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WINDow TA")
 
-    def adjust_lower_limit_of_trigger_level(self):
+    def set_adjust_lower_limit_of_trigger_level(self) -> None:
         """
         **Rigol Programming Guide**
 
@@ -532,9 +613,9 @@ class SlopeWindow(SubSubController):
         :TRIGger:SLOPe:WINDow TB
         The query returns TB.
         """
-        raise NotImplementedError()
+        self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WINDow TB")
 
-    def adjust_upper_and_lower_limit_of_trigger_level(self):
+    def set_adjust_upper_and_lower_limit_of_trigger_level(self) -> None:
         """
         **Rigol Programming Guide**
 
@@ -580,7 +661,55 @@ class SlopeWindow(SubSubController):
         :TRIGger:SLOPe:WINDow TB
         The query returns TB.
         """
-        raise NotImplementedError()
+        self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WINDow TAB")
+
+    def status(self) -> str:
+        """
+        **Rigol Programming Guide**
+
+        **Syntax**
+
+        :TRIGger:SLOPe:WINDow <window>
+        :TRIGger:SLOPe:WINDow?
+
+        **Description**
+
+        Set the type of the vertical window in slope trigger.
+        Query the current type of the vertical window in slope trigger.
+
+        **Parameter**
+
+        ========= ========= ============ =======
+        Name      Type      Range        Default
+        ========= ========= ============ =======
+        <window>  Discrete  {TA|TB|TAB}  TA
+        ========= ========= ============ =======
+
+        **Explanation**
+
+        Different vertical windows correspond to different trigger level
+        adjustment modes.
+
+        TA: only adjust the upper limit of the trigger level. Refer to the
+        :TRIGger:SLOPe:ALEVel command.
+
+        TB: only adjust the lower limit of the trigger level. Refer to the
+        :TRIGger:SLOPe:BLEVel command.
+
+        TAB: adjust the upper and lower limits of the trigger level at the
+        same time. Refer to the :TRIGger:SLOPe:ALEVel and :TRIGger:SLOPe:BLEVel
+        commands.
+
+        **Return Format**
+
+        The query returns TA, TB or TAB.
+
+        **Example**
+
+        :TRIGger:SLOPe:WINDow TB
+        The query returns TB.
+        """
+        return self.subsubdevice.subdevice.device.ask(":TRIGger:SLOPe:WINDow?")
 
 
 class Slope(SubController):
@@ -589,7 +718,7 @@ class Slope(SubController):
         self.when: SlopeWhen = SlopeWhen(self)
         self.window: SlopeWindow = SlopeWindow(self)
 
-    def source(self):
+    def set_source(self, channel: int = 1) -> None:
         """
         **Rigol Programming Guide**
 
@@ -620,9 +749,43 @@ class Slope(SubController):
         :TRIGger:SLOPe:SOURce CHANnel2
         The query returns CHAN2.
         """
-        raise NotImplementedError()
+        check_input(channel, "channel", int, 1, 2)
+        self.subdevice.device.ask(":TRIGger:SLOPe:SOURce CHANnel{channel}")
 
-    def upper_limit(self):
+    def get_source(self) -> str:
+        """
+        **Rigol Programming Guide**
+
+        **Syntax**
+
+        :TRIGger:SLOPe:SOURce <source>
+        :TRIGger:SLOPe:SOURce?
+
+        **Description**
+
+        Select the trigger source of slope trigger.
+        Query the current trigger source of slope trigger.
+
+        **Parameter**
+
+        ========= ========= ==================== ========
+        Name      Type      Range                Default
+        ========= ========= ==================== ========
+        <source>  Discrete  {CHANnel1|CHANnel2}  CHANnel1
+        ========= ========= ==================== ========
+
+        **Return Format**
+
+        The query returns CHAN1 or CHAN2.
+
+        **Example**
+
+        :TRIGger:SLOPe:SOURce CHANnel2
+        The query returns CHAN2.
+        """
+        return self.subdevice.device.ask(":TRIGger:SLOPe:SOURce?")
+
+    def set_upper_limit(self, time: float = 2.0e-6) -> None:
         """
         **Rigol Programming Guide**
 
@@ -661,9 +824,51 @@ class Slope(SubController):
         :TRIGger:SLOPe:TUPPer 0.000003
         The query returns 3.000000e-06.
         """
-        raise NotImplementedError()
+        check_input(time, "time", float, 10.0e-9, 2.0, "s")
+        self.subdevice.device.ask(f":TRIGger:SLOPe:TUPPer {time}")
 
-    def lower_limit(self):
+    def get_upper_limit(self) -> float:
+        """
+        **Rigol Programming Guide**
+
+        **Syntax**
+
+        :TRIGger:SLOPe:TUPPer <time>
+        :TRIGger:SLOPe:TUPPer?
+
+        **Description**
+
+        Set the upper limit of time in slope trigger and the unit is s.
+        Query the current upper limit of time in slope trigger.
+
+        **Parameter**
+
+        ======= ===== =========== =======
+        Name    Type  Range       Default
+        ======= ===== =========== =======
+        <time>  Real  10ns to 1s  2μs
+        ======= ===== =========== =======
+
+        Note: when the trigger condition is PGLess or NGLess, the range is
+        from 20ns to 1s.
+
+        **Explanation**
+
+        This command is only available when the trigger condition (refer to the
+        :TRIGger:SLOPe:WHEN command) is PLESs, NLESs, PGLess or NGLess.
+
+        **Return Format**
+
+        The query returns the upper limit of time in scientific notation.
+
+        **Example**
+
+        :TRIGger:SLOPe:TUPPer 0.000003
+        The query returns 3.000000e-06.
+        """
+        return float(self.subdevice.device.ask(":TRIGger:SLOPe:TUPPer?"))
+
+    def set_lower_limit(self, time: float = 1.0e-6) -> None:
         """
         **Rigol Programming Guide**
 
@@ -702,9 +907,51 @@ class Slope(SubController):
         :TRIGger:SLOPe:TLOWer 0.000003
         The query returns 3.000000e-06.
         """
-        raise NotImplementedError()
+        check_input(time, "time", float, 10.0e-9, 1.0, "s")
+        self.subdevice.device.ask(f":TRIGger:SLOPe:TLOWer {time}")
 
-    def upper_limit_trigger_level(self):
+    def get_lower_limit(self) -> float:
+        """
+        **Rigol Programming Guide**
+
+        **Syntax**
+
+        :TRIGger:SLOPe:TLOWer <time>
+        :TRIGger:SLOPe:TLOWer?
+
+        **Description**
+
+        Set the lower limit of time in slope trigger and the unit is s.
+        Query the current lower limit of time in slope trigger.
+
+        **Parameter**
+
+        ======= ===== =========== =======
+        Name    Type  Range       Default
+        ======= ===== =========== =======
+        <time>  Real  10ns to 1s  1μs
+        ======= ===== =========== =======
+
+        Note: when the trigger condition is PGLess or NGLess, the range is
+        from 10ns to 999ms.
+
+        **Explanation**
+
+        This command is only available when the trigger condition (refer to the
+        :TRIGger:SLOPe:WHEN command) is PGReater, NGReater, PGLess or NGLess.
+
+        **Return Format**
+
+        The query returns the lower limit of time in scientific notation.
+
+        **Example**
+
+        :TRIGger:SLOPe:TLOWer 0.000003
+        The query returns 3.000000e-06.
+        """
+        return float(self.subdevice.device.ask(":TRIGger:SLOPe:TLOWer?"))
+
+    def set_upper_limit_trigger_level(self, level: float = 0.0) -> None:
         """
         **Rigol Programming Guide**
 
@@ -742,9 +989,61 @@ class Slope(SubController):
         :TRIGger:SLOPe:ALEVel 0.16
         The query returns 1.600000e-01.
         """
-        raise NotImplementedError()
+        scale: float = -1.0
+        offset: float = -1.0
+        channel: str = self.get_source()
+        if channel == "CHANnel1":
+            scale = self.subdevice.device.channel1.get_scale()
+            offset = self.subdevice.device.channel1.get_offset()
+        elif channel == "CHANnel2":
+            scale = self.subdevice.device.channel2.scale()
+            offset = self.subdevice.device.channel2.get_offset()
+        else:
+            raise RuntimeError("The oscilloscope returned an unknown channel")
+        check_level(level, scale, offset)
+        self.subdevice.device.ask(":TRIGger:SLOPe:ALEVel {level}")
 
-    def lower_limit_trigger_level(self):
+    def get_upper_limit_trigger_level(self) -> float:
+        """
+        **Rigol Programming Guide**
+
+        **Syntax**
+
+        :TRIGger:SLOPe:ALEVel <level>
+        :TRIGger:SLOPe:ALEVel?
+
+        **Description**
+
+        Set the upper limit of the trigger level in slope trigger and the unit
+        is the same with the current amplitude unit.
+        Query the current upper limit of the trigger level in slope trigger.
+
+        **Parameter**
+
+        ======== ===== =========================== =======
+        Name     Type  Range                       Default
+        ======== ===== =========================== =======
+        <level>  Real  ± 5 × VerticalScale from    0
+                       the screen center - OFFSet
+        ======== ===== =========================== =======
+
+        Note:
+        For the VerticalScale, refer to the :CHANnel<n>:SCALe command.
+        For the OFFSet, refer to the :CHANNel<n>:OFFSet command.
+
+        **Return Format**
+
+        The query returns the upper limit of the trigger level in scientific
+        notation.
+
+        **Example**
+
+        :TRIGger:SLOPe:ALEVel 0.16
+        The query returns 1.600000e-01.
+        """
+        return float(self.subdevice.device.ask(":TRIGger:SLOPe:ALEVel?"))
+
+    def set_lower_limit_trigger_level(self, level: float = 0.0) -> None:
         """
         **Rigol Programming Guide**
 
@@ -782,4 +1081,56 @@ class Slope(SubController):
         :TRIGger:SLOPe:BLEVel 0.16
         The query returns 1.600000e-01.
         """
-        raise NotImplementedError()
+        scale: float = -1.0
+        offset: float = -1.0
+        channel: str = self.get_source()
+        if channel == "CHANnel1":
+            scale = self.subdevice.device.channel1.get_scale()
+            offset = self.subdevice.device.channel1.get_offset()
+        elif channel == "CHANnel2":
+            scale = self.subdevice.device.channel2.scale()
+            offset = self.subdevice.device.channel2.get_offset()
+        else:
+            raise RuntimeError("The oscilloscope returned an unknown channel")
+        check_level(level, scale, offset)
+        self.subdevice.device.ask(f":TRIGger:SLOPe:BLEVel {level}")
+
+    def get_lower_limit_trigger_level(self) -> float:
+        """
+        **Rigol Programming Guide**
+
+        **Syntax**
+
+        :TRIGger:SLOPe:BLEVel <level>
+        :TRIGger:SLOPe:BLEVel?
+
+        **Description**
+
+        Set the lower limit of the trigger level in slope trigger and the unit
+        is the same with the current amplitude unit.
+        Query the current lower limit of the trigger level in slope trigger.
+
+        **Parameter**
+
+        ======== ===== =========================== =======
+        Name     Type  Range                       Default
+        ======== ===== =========================== =======
+        <level>  Real  ± 5 × VerticalScale from    0
+                       the screen center - OFFSet
+        ======== ===== =========================== =======
+
+        Note:
+        For the VerticalScale, refer to the :CHANnel<n>:SCALe command.
+        For the OFFSet, refer to the :CHANNel<n>:OFFSet command.
+
+        **Return Format**
+
+        The query returns the lower limit of the trigger level in scientific
+        notation.
+
+        **Example**
+
+        :TRIGger:SLOPe:BLEVel 0.16
+        The query returns 1.600000e-01.
+        """
+        return float(self.subdevice.device.ask(":TRIGger:SLOPe:BLEVel?"))
