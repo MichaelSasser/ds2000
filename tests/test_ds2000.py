9dd96b8d6db2e8f8a9ac654e37a8a10c740a7753
Michael Sasser <Michael@MichaelSasser.org>
2021-01-15 04:25:00 +0100

    Add test semantic vsersion

---
+++ tests/test_ds2000.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# ds2000 - The Python Library for Rigol DS2000 Oscilloscopes
+# Copyright (c) 2021  Michael Sasser <Michael@MichaelSasser.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import annotations
+
+import re
+
+from matrixctl import __version__
+
+__author__: str = "Michael Sasser"
+__email__: str = "Michael@MichaelSasser.org"
+
+
+def test_version() -> None:
+    """Test, if the version matches SemVer.
+
+    The regular expression is form:
+    https://semver.org/#is-there-a-suggested-regular-expression-regex-to-
+            check-a-semver-string
+    """
+    # Setup
+    desired = re.compile(
+        r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\."
+        r"(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-]"
+        r"[0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?"
+        r"(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+    )
+
+    # Exercise
+    actual = __version__
+
+    # Verify
+    assert desired.fullmatch(actual)
+
+    # Cleanup - None
+
+
+# vim: set ft=python :
